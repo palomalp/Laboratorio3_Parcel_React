5f6196883dcc57eea985fbd3ed0ca604df4f20c7
PalomaLP <palomaledesmap@gmail.com>
2021-01-14 13:21:44 +0100

    first version

--- src/hello.tsx
+++ src/hello.tsx
@@ -6,7 +6,7 @@ export const HelloComponent: React.FC = () => {
   return (
     <div className={classes.hello}>
       <img src={logo} className={classes.logo}/>
-      <h2>Hello from React</h2>      
+      <h2>Hello from React!</h2>      
     </div>
   );
 };
